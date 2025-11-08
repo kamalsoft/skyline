87d8f199a5d493bb4879744e827b92ac71de54a5
Kamalesh Kothandan <kamalsoft@gmail.com>
2025-11-07 19:12:16 -0600

    removed the unused components

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,7 +4,6 @@
 // See https://developers.google.com/web/tools/workbox/
 
 import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
